b1eb4ed3ec5f94601b12566c0914b9bc30ec6023
ThomasF22 <tfurfaro9@gmail.com>
2026-03-19 17:42:11 +0100

    Fix base path for correct repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/corsidonie-app/',
+  base: '/challenge-innovation-2026-corsidonie/',
   plugins: [vue()],
   server: {
     port: 5173
